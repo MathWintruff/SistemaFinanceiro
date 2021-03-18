ea213da41f86c262ec54228f6059876f087e6f5d
MathWintruff <mathwintruffac@gmail.com>
2021-03-18 20:11:30 -0300

    Enable and disable Confirm button added

--- SistemaFinanceiro.py
+++ SistemaFinanceiro.py
@@ -4,7 +4,9 @@ from SistemaFinanceiro_UI import Ui_Form
 #region Ui and UiFunctions
 
 def AddConnects(self):
-        self.BtnConfirma.clicked.connect(self.SetTableContent)
+        #self.BtnConfirma.clicked.connect()
+        self.TbwEmprestimos.itemSelectionChanged.connect(self.EnableConfirmButton)
+        self.BtnProcura.clicked.connect(self.SetTableContent)
 Ui_Form.AddConnects = AddConnects
 
 
@@ -17,15 +19,33 @@ Ui_Form.SetCmbItens = SetCmbItens
         
 
 def SetTableContent(self):
+    self.TxbSenha.setEnabled(False)
+    self.BtnConfirma.setEnabled(False)
     emprestimos = GetEmprestimosRelacionados(self.CmbCredor.currentText(), self.CmbDevedor.currentText())
     self.TbwEmprestimos.setRowCount(len(emprestimos))
+
     for i, emprestimo in enumerate(emprestimos):
-        self.TbwEmprestimos.setItem(i, 0, QtWidgets.QTableWidgetItem(emprestimo[0]))
-        self.TbwEmprestimos.setItem(i, 1, QtWidgets.QTableWidgetItem(emprestimo[1]))
-        self.TbwEmprestimos.setItem(i, 2, QtWidgets.QTableWidgetItem(emprestimo[2]))
-        self.TbwEmprestimos.setItem(i, 3, QtWidgets.QTableWidgetItem(str(emprestimo[3])))
+        credor = QtWidgets.QTableWidgetItem(emprestimo[0])
+        credor.setFlags(QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsUserCheckable|QtCore.Qt.ItemIsEnabled)
+        devedor = QtWidgets.QTableWidgetItem(emprestimo[1])
+        devedor.setFlags(QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsUserCheckable|QtCore.Qt.ItemIsEnabled)
+        data = QtWidgets.QTableWidgetItem(emprestimo[2])
+        data.setFlags(QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsUserCheckable|QtCore.Qt.ItemIsEnabled)
+        valor = QtWidgets.QTableWidgetItem(emprestimo[3])
+        valor.setFlags(QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsUserCheckable|QtCore.Qt.ItemIsEnabled)
+
+        self.TbwEmprestimos.setItem(i, 0, credor)
+        self.TbwEmprestimos.setItem(i, 1, devedor)
+        self.TbwEmprestimos.setItem(i, 2, data)
+        self.TbwEmprestimos.setItem(i, 3, valor)
+
     self.TbwEmprestimos
 Ui_Form.SetTableContent = SetTableContent
+
+def EnableConfirmButton(self):
+    self.TxbSenha.setEnabled(True)
+    self.BtnConfirma.setEnabled(True)
+Ui_Form.EnableConfirmButton = EnableConfirmButton
 
 def ExecAllExtFunctions(self):
     self.AddConnects()
